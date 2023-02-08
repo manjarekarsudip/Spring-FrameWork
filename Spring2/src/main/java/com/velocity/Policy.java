9c4c216208c2d6423ed2de77cb827477059a1ae3
sudip <manjarekarsudip@gmail.com>
2023-02-08 22:29:28 +0530

    autowire - byType

---
+++ Spring2/src/main/java/com/velocity/Policy.java
@@ -0,0 +1,21 @@
+package com.velocity;
+
+public class Policy {
+
+	private String planName;
+	private String planAmount;
+	public String getPlanName() {
+		return planName;
+	}
+	public void setPlanName(String planName) {
+		this.planName = planName;
+	}
+	public String getPlanAmount() {
+		return planAmount;
+	}
+	public void setPlanAmount(String planAmount) {
+		this.planAmount = planAmount;
+	}
+	
+	
+}
