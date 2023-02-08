fcde05979cd66e413bddb6c3a682c9710ea728c2
sudip <manjarekarsudip@gmail.com>
2023-02-08 22:27:45 +0530

    IOC Concept

---
+++ Spring1/src/main/java/com/test/Student.java
@@ -0,0 +1,31 @@
+package com.test;
+
+public class Student {
+
+	private int age;
+	private String name;
+	private String city;
+	
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	@Override
+	public String toString() {
+		return "Student [age=" + age + ", name=" + name + ", city=" + city + "]";
+	}	
+}
